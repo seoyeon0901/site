d51b07f7210862302d7e85bfec6d3cfc5b7b1ced
Seoyeon <99859378+seoyeon0901@users.noreply.github.com>
2023-02-22 16:06:14 +0900

    login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,49 @@
+import { useState } from 'react';
 import './Login.scss';
 
 const Login = () => {
+	const [id, setId] = useState('');
+	const [password, setPassword] = useState('');
+	const onchangeid = (e: any) => {
+		setId(e.target.value);
+	};
+	const onchangepassword = (e: any) => {
+		setPassword(e.target.value);
+	};
+
+	const onsubmit = (e: any) => {
+		setId('');
+		setPassword('');
+	};
 	return (
 		<div className="login">
 			<div className="loginbox">
-				<form>
-					<label>
-						안녕하세요
-						<br />
-						<strong>황서연의 블로그</strong> 입니다
-					</label>
+				<div className="red-circle" />
+				<div className="yellow-circle" />
+				<div className="green-circle" />
+				<form onSubmit={onsubmit}>
+					<div className="info">
+						<p>안녕하세요</p> <strong>OUR PAGE</strong>입니다.
+					</div>
 					<div className="inputplace">
-						<input placeholder="이메일" />
-						<input placeholder="비밀번호" />
+						<input
+							type="text"
+							placeholder="아이디(이메일)을 입력해 주세요"
+							value={id}
+							onChange={onchangeid}
+						/>
+						<input
+							type="password"
+							placeholder="비밀번호를 입력해 주세요"
+							value={password}
+							onChange={onchangepassword}
+						/>
 					</div>
 					<div className="button">
-						<button>확인</button>
+						<button type="submit" className="assign">
+							확인
+						</button>
+						<button className="join">회원가입</button>
 					</div>
 				</form>
 			</div>
